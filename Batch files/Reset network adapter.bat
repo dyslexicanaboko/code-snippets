2276d0801d6baadfca715df2aaac41e739270a15
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2021-06-17 21:23:11 -0400

    Adding batch file for resetting network adapter

---
+++ Batch files/Reset network adapter.bat
@@ -0,0 +1,6 @@
+REM Run as administrator
+REM netsh interface show interface REM display interfaces
+netsh interface set interface Ethernet0 disable
+netsh interface set interface Ethernet0 enable
+
+REM PAUSE

22e5b0c73fdb785d0151bc6a5a9ac9714a1ed3e8
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2021-10-16 23:32:46 -0400

    Dapper tests are finished

--- Visual C#/BasicDataLayers/BasicDataLayers.Lib/DapperFlavor/DapperRepository.cs
+++ Visual C#/BasicDataLayers/BasicDataLayers.Lib/DapperFlavor/DapperRepository.cs
@@ -1,19 +1,21 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using BasicDataLayers.Lib.Entities;
 
 namespace BasicDataLayers.Lib.DapperFlavor
 {
-    public class DapperRepository
+	public class DapperRepository
 		: BaseDal
 	{
 		public RudimentaryEntity Select(int primaryKey)
 		{
 			var sql = @"
 			SELECT
-				 DollarAmount
+				 PrimaryKey
+				,DollarAmount
 				,ForeignKey
 				,IsYes
 				,Label
@@ -59,7 +61,7 @@ namespace BasicDataLayers.Lib.DapperFlavor
 			}
 		}
 
-		public void Insert(RudimentaryEntity entity)
+		public int Insert(RudimentaryEntity entity)
 		{
 			var sql = @"INSERT INTO dbo.RudimentaryEntity (
 				 DollarAmount
@@ -77,12 +79,14 @@ namespace BasicDataLayers.Lib.DapperFlavor
 				,@LuckyNumber
 				,@MathCalculation
 				,@ReferenceId
-				,@RightNow)";
+				,@RightNow);
 
-            using (var connection = new SqlConnection(ConnectionString))
-            {
-                connection.Execute(sql, entity);
-            }
+				SELECT SCOPE_IDENTITY() AS PK;";
+
+			using (var connection = new SqlConnection(ConnectionString))
+			{
+				return connection.ExecuteScalar<int>(sql, entity);
+			}
 		}
 
 		public void Update(RudimentaryEntity entity)
@@ -98,10 +102,23 @@ namespace BasicDataLayers.Lib.DapperFlavor
 						,RightNow = @RightNow
 					WHERE PrimaryKey = @PrimaryKey";
 
-            using (var connection = new SqlConnection(ConnectionString))
-            {
-                connection.Execute(sql, entity);
-            }
+			using (var connection = new SqlConnection(ConnectionString))
+			{
+				var p = new DynamicParameters();
+				//Sizes should be filled in where possible because it WILL make a difference depending on the parameter type
+				//DateTime2 for example will not get the precision desired if you do not put in the number for precision
+				p.Add("@PrimaryKey", dbType: DbType.Int32, value: entity.PrimaryKey);
+				p.Add("@ForeignKey", dbType: DbType.Int32, value: entity.ForeignKey);
+				p.Add("@ReferenceId", dbType: DbType.Guid, value: entity.ReferenceId);
+				p.Add("@IsYes", dbType: DbType.Boolean, value: entity.IsYes);
+				p.Add("@LuckyNumber", dbType: DbType.Int32, value: entity.LuckyNumber);
+				p.Add("@DollarAmount", dbType: DbType.Decimal, value: entity.DollarAmount);
+				p.Add("@MathCalculation", dbType: DbType.Double, value: entity.MathCalculation);
+				p.Add("@Label", dbType: DbType.String, value: entity.Label);
+				p.Add("@RightNow", dbType: DbType.DateTime2, size: 7, value: entity.RightNow);
+
+				connection.Execute(sql, p);
+			}
 		}
 	}
 }
---
+++ Visual C#/BasicDataLayers/BasicDataLayers.Tests/DapperFlavorTests.cs
@@ -0,0 +1,28 @@
+﻿using BasicDataLayers.Lib.DapperFlavor;
+using BasicDataLayers.Lib.Entities;
+using NUnit.Framework;
+
+namespace BasicDataLayers.Tests
+{
+    [TestFixture]
+    public class DapperFlavorTests
+        : BaseTest
+    {
+        [Test]
+        public void InsertUpdateSelect()
+        {
+            var e = new RudimentaryEntity();
+            var dapper = new DapperRepository();
+
+            var pk = dapper.Insert(e);
+
+            UpdateObject(e, pk);
+
+            dapper.Update(e);
+
+            var a = dapper.Select(e.PrimaryKey);
+
+            AssertAreEqual(e, a);
+        }
+    }
+}

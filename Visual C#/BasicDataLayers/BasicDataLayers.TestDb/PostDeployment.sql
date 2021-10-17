638398d208d321da4eab281ea74d790b40873ff7
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2021-10-16 20:48:43 -0400

    Missed staged files.

---
+++ Visual C#/BasicDataLayers/BasicDataLayers.TestDb/PostDeployment.sql
@@ -0,0 +1,36 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+			   SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+IF NOT EXISTS(SELECT TOP(1) 1 FROM dbo.RudimentaryEntity WHERE PrimaryKey = 1)
+BEGIN
+	INSERT INTO dbo.RudimentaryEntity
+	(
+		ForeignKey
+	   ,ReferenceId
+	   ,IsYes
+	   ,LuckyNumber
+	   ,DollarAmount
+	   ,MathCalculation
+	   ,Label
+	   ,RightNow
+	)
+	VALUES
+	(   1             -- ForeignKey - int
+	   ,NEWID()          -- ReferenceId - uniqueidentifier
+	   ,1          -- IsYes - bit
+	   ,7             -- LuckyNumber - int
+	   ,124.67          -- DollarAmount - decimal(18, 2)
+	   ,3.142738495893783929           -- MathCalculation - float
+	   ,'Rudimentary entry'            -- Label - varchar(500)
+	   ,SYSDATETIME() -- RightNow - datetime2(7)
+	)
+END
---
+++ Visual C#/BasicDataLayers/BasicDataLayers.TestDb/RudimentaryEntity.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[RudimentaryEntity]
+(
+	 PrimaryKey int IDENTITY(1, 1) NOT NULL
+	,ForeignKey int NOT NULL
+	,ReferenceId UNIQUEIDENTIFIER NOT NULL
+	,IsYes BIT NOT NULL
+	,LuckyNumber int NOT NULL
+	,DollarAmount decimal(18, 2) NOT NULL
+	,MathCalculation float NOT NULL
+	,Label VARCHAR(500) NOT NULL
+	,RightNow DateTime2(7) NOT NULL
+)

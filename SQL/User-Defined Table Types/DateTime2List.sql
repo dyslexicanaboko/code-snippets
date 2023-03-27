3b682469f84535d6aa3f59e488ec7eec7a8337f5
Eli Hayon <dyslexicanaboko@users.noreply.github.com>
2023-03-26 22:28:38 -0400

    Uploading some files I don't want to have to write ever again. (#48)
    1. `User-defined table types` of various generic C# types
    2. ResultBase.cs - like it sounds a base file for results.

---
+++ SQL/User-Defined Table Types/DateTime2List.sql
@@ -0,0 +1,5 @@
+﻿CREATE TYPE [dbo].[DateTime2List] AS TABLE
+(
+	DateTime2Value DateTime2(7) NOT NULL,
+	UNIQUE CLUSTERED (DateTime2Value)
+)
---
+++ SQL/User-Defined Table Types/NVarcharList.sql
@@ -0,0 +1,4 @@
+﻿CREATE TYPE [dbo].[NVarcharList] AS TABLE
+(
+	NVarcharValue NVARCHAR(MAX) NOT NULL
+)
